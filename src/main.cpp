0419c3fca4678fc722dcb544952a2d1b5aa77f84
LakshayBindal574 <lakshay.bindal_cs.aiml24@gla.ac.in>
2026-02-27 16:30:02 +0530

    Initializing analog write for Color

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,9 @@ void setup() {
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
 }
+
+void setColor(int red, int green, int blue) {
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
+}
